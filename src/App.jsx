6335a7c7fcaeeb3717b3235f49ca93089002ad08
HerodesD <herodesdark@gmail.com>
2024-10-04 19:14:21 -0400

    inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Home from "./Pages/Home"
+import Details from "./Pages/Details"
+import NotFound from "./Pages/NotFound"
+
+const router = createBrowserRouter([]);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+
+  return (
+
+    <>
+
+
+
+    </>
+  );
+}
+
+
+
+
+
+
+
+export default App
